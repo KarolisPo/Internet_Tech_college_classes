5decc1b6c561a8a91ab2540cdb31a99b3c6a6930
KarolisPo <73240914+KarolisPo@users.noreply.github.com>
2021-11-19 09:39:26 +0200

    Add files via upload

---
+++ 16PD_Karolis_Pociūnas/js/script3.js
@@ -0,0 +1,10 @@
+document.body.style.backgroundColor = 'red';
+
+var pav = document.getElementById('pavadinimas');
+pav.style.color='blue';
+
+var txtclr = document.getElementById('txtclr');
+txtclr.style.color='white'
+
+
+alert('Sveiki atvykę į trečią puslapį');
